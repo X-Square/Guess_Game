3d873519b744e0f147645761ea99c6fe73af6a89
X-Square <xiaoxuanbusiness@gmail.com>
2018-02-13 10:54:35 -0500

    Wrote the functioning Bash program.

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,18 @@
-echo "hello world"
+echo "Guess how many files are in the current directory. Type a number:"
+
+read  guess
+ans=$(ls | wc -l)
+
+while [[ $guess -ne $ans ]]
+do
+	if [[ $guess -gt $ans ]]
+	then
+		echo "Your guess is too high. Guess again:"
+		read guess
+	else
+		echo "Your guess is too low. Guess again:"
+		read guess
+	fi
+done
+
+echo "That is right. Congratulations!"
